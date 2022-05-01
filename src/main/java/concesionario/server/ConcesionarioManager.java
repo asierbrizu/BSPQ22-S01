4308a7ecc8de78dc763c3de2cf78af8afb17774e
Iker Morales <ikermorales@opendeusto.es>
2022-05-01 15:14:30 +0200

    Ventana del Servidor funcionando

--- src/main/java/concesionario/server/ConcesionarioManager.java
+++ src/main/java/concesionario/server/ConcesionarioManager.java
@@ -5,13 +5,16 @@ import java.awt.Font;
 import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
@@ -28,21 +31,23 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import concesionario.clases.Compra;
+import concesionario.server.bd.BD;
+import concesionario.server.bd.DBException;
 import concesionario.util.CompraException;
 
 public class ConcesionarioManager extends JFrame implements ActionListener, Runnable {
-	
+
 	private JList listaCompras;
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
 	private JTextField textField_3;
 	private JTextField textField_4;
-	
+
 	private ArrayList<Compra> compras = new ArrayList<>();
 	String[] arrayParaLaJList;
 	private HashMap<String, Compra> hashCompras;
-	
+
 	private Client client;
 	private WebTarget webTarget;
 
@@ -57,7 +62,7 @@ public class ConcesionarioManager extends JFrame implements ActionListener, Runn
 		setBounds(100, 100, 315, 468);	
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		getContentPane().setLayout(null);
-		
+
 
 		arrayParaLaJList = new String[compras.size()];
 		hashCompras = new HashMap<>();
@@ -67,8 +72,8 @@ public class ConcesionarioManager extends JFrame implements ActionListener, Runn
 			hashCompras.put(compra.getId(), compra);
 			i++;
 		}
-		
-		
+
+
 		listaCompras = new JList(arrayParaLaJList);
 		listaCompras.setBackground(SystemColor.inactiveCaption);
 		listaCompras.setBounds(10, 88, 283, 102);
@@ -76,89 +81,75 @@ public class ConcesionarioManager extends JFrame implements ActionListener, Runn
 		scrollPane.setBounds(10, 88, 283, 102);
 		scrollPane.setViewportView(listaCompras);
 		getContentPane().add(scrollPane);
-		
-		listaCompras.addListSelectionListener(new ListSelectionListener() {
-			
-			@Override
-			public void valueChanged(ListSelectionEvent e) {
-				if (!e.getValueIsAdjusting()) {
-						textField.setText(hashCompras.get(listaCompras.getSelectedValue()).getId());
-						textField_1.setText(hashCompras.get(listaCompras.getSelectedValue()).getMatricula());
-						textField_2.setText(hashCompras.get(listaCompras.getSelectedValue()).getCliente().getNombre() + " " + hashCompras.get(listaCompras.getSelectedValue()).getCliente().getApellido());
-						textField_3.setText(hashCompras.get(listaCompras.getSelectedValue()).getFecha());
-						textField_4.setText(hashCompras.get(listaCompras.getSelectedValue()).getId_coche());
-						repaint();
-						validate();
-	                }
-			}
-		});
-		
+
+
 		JLabel titulo = new JLabel("Ventas ADAIA");
 		titulo.setFont(new Font("Monospaced", Font.PLAIN, 24));
 		titulo.setHorizontalAlignment(SwingConstants.CENTER);
 		titulo.setBounds(10, 11, 283, 66);
 		getContentPane().add(titulo);
-		
-		
+
+
 		JPanel panelInformacion = new JPanel();
 		panelInformacion.setBackground(new Color(255, 255, 255));
 		panelInformacion.setBorder(new TitledBorder(null, "Informacion: ", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panelInformacion.setBounds(10, 201, 283, 221);
 		getContentPane().add(panelInformacion);
 		panelInformacion.setLayout(null);
-		
+
 		JLabel idlbl = new JLabel("ID: ");
 		idlbl.setBounds(10, 32, 48, 14);
 		panelInformacion.add(idlbl);
-		
+
 		JLabel matriculalbl = new JLabel("Matricula: ");
 		matriculalbl.setBounds(10, 69, 66, 14);
 		panelInformacion.add(matriculalbl);
-		
+
 		JLabel clientelbl = new JLabel("Cliente: ");
 		clientelbl.setBounds(10, 110, 48, 14);
 		panelInformacion.add(clientelbl);
-		
+
 		JLabel fechalbl = new JLabel("Fecha: ");
 		fechalbl.setBounds(10, 153, 48, 14);
 		panelInformacion.add(fechalbl);
-		
+
+
 		textField = new JTextField();
 		textField.setEditable(false);
 		textField.setBounds(83, 29, 190, 20);
 		panelInformacion.add(textField);
 		textField.setColumns(10);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setEditable(false);
 		textField_1.setColumns(10);
 		textField_1.setBounds(83, 66, 190, 20);
 		panelInformacion.add(textField_1);
-		
+
 		textField_2 = new JTextField();
 		textField_2.setEditable(false);
 		textField_2.setColumns(10);
 		textField_2.setBounds(83, 107, 190, 20);
 		panelInformacion.add(textField_2);
-		
+
 		textField_3 = new JTextField();
 		textField_3.setEditable(false);
 		textField_3.setColumns(10);
 		textField_3.setBounds(83, 147, 190, 20);
 		panelInformacion.add(textField_3);
-		
+
 		textField_4 = new JTextField();
 		textField_4.setEditable(false);
 		textField_4.setColumns(10);
 		textField_4.setBounds(83, 190, 190, 20);
 		panelInformacion.add(textField_4);
-		
+
 		JLabel cochelbl = new JLabel("Coche id:");
 		cochelbl.setBounds(10, 193, 66, 14);
 		panelInformacion.add(cochelbl);
-		
+
 		setVisible(true);
-		
+
 		thread = new Thread(this);
 		thread.start();
 	}
@@ -174,37 +165,61 @@ public class ConcesionarioManager extends JFrame implements ActionListener, Runn
 			throw new CompraException("" + response.getStatus());
 		}
 	}
-	
+
 	public void run() {
 		running.set(true);
 		while(running.get()) {
-			try { 
-				Thread.sleep(2000);
+			try {
+
+				Connection con = BD.initBD("concesionario.db");
+				compras = BD.obtenerListaCompras(con);
+
+				Thread.sleep(5000);
 				System.out.println("Obtaining data from server...");
-					//Cosas que hace el server
-				
-					try {
-						compras=getCompras();
-						
-						arrayParaLaJList = new String[compras.size()];
-						hashCompras = new HashMap<>();
-						int i = 0;
-						for (Compra compra : compras) {
-							arrayParaLaJList[i] = compra.getId();
-							hashCompras.put(compra.getId(), compra);
-							i++;
+
+				//Cosas que hace el server
+
+				try {					
+					arrayParaLaJList = new String[compras.size()];
+					hashCompras = new HashMap<>();
+					int i = 0;
+					for (Compra compra : compras) {
+						arrayParaLaJList[i] = compra.getId();
+						hashCompras.put(compra.getId(), compra);
+						i++;
+					}			
+
+					listaCompras.addListSelectionListener(new ListSelectionListener() {
+
+						@Override
+						public void valueChanged(ListSelectionEvent e) {
+							if (!e.getValueIsAdjusting()) {
+								textField.setText(hashCompras.get(listaCompras.getSelectedValue()).getId());
+								textField_1.setText(hashCompras.get(listaCompras.getSelectedValue()).getMatricula());
+								textField_2.setText(hashCompras.get(listaCompras.getSelectedValue()).getCliente().getNombre() + " " + hashCompras.get(listaCompras.getSelectedValue()).getCliente().getApellido());
+								textField_3.setText(hashCompras.get(listaCompras.getSelectedValue()).getFecha());
+								textField_4.setText(hashCompras.get(listaCompras.getSelectedValue()).getId_coche());
+								repaint();
+								validate();
+							}
 						}
-						
-						repaint();
-						validate();
-						
-					} catch (Exception e) {
-					}
-				
-            } catch (InterruptedException e){ 
-                Thread.currentThread().interrupt();
-                System.out.println("Thread was interrupted, Failed to complete operation");
-            }
+					});
+
+					listaCompras.setListData(arrayParaLaJList);
+
+
+					repaint();
+					validate();
+
+					BD.closeBD(con);
+
+				} catch (DBException | NullPointerException e1) {
+				}
+			} catch (NullPointerException e) {
+			} catch (InterruptedException | DBException e) {
+				Thread.currentThread().interrupt();
+				System.out.println("Thread was interrupted, Failed to complete operation");
+			}
 		}
 	}
 
@@ -219,9 +234,10 @@ public class ConcesionarioManager extends JFrame implements ActionListener, Runn
 		new ConcesionarioManager(hostname, port);
 	}
 
-
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		//Acciones de los botones
+		// TODO Auto-generated method stub
+		
 	}
+
 }
