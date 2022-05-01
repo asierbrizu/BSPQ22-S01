d25f012af5f7359bc32b7a2a74f9b0b2c94fc588
AsierMM <alazne.parra@opendeusto.es>
2022-05-02 00:55:23 +0200

    ConcesionarioManagerTest

---
+++ src/test/java/concesionario/clases/ConcesionarioManagerTest.java
@@ -0,0 +1,20 @@
+package concesionario.clases;
+
+import static org.junit.Assert.*; 
+
+import org.junit.Test;
+
+import concesionario.client.VentanaAdministrador;
+import concesionario.server.ConcesionarioManager;
+
+public class ConcesionarioManagerTest {
+
+	@Test
+	public void test() {
+		try {
+			ConcesionarioManager cM = new ConcesionarioManager("127.0.0.1", "8080");
+		} catch (Exception e) {
+		}
+	}
+
+}
