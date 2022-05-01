2f8c219b4418114f8d48c337b0dc4609fea95ee4
Iker Morales <ikermorales@opendeusto.es>
2022-05-01 21:25:15 +0200

    DBExceptionTest realizado.

--- src/test/java/concesionario/clases/DBExcpetionTest.java
+++ src/test/java/concesionario/clases/DBExcpetionTest.java
@@ -8,13 +8,35 @@ import org.junit.Test;
 public class DBExcpetionTest {
 
 	@Test
-	public void DBException() {
+	public void DBExceptionTest() {
 		DBException dbe = null;
 		try {
 			dbe = new DBException("Error");
 		} catch (concesionario.server.bd.DBException e) {
 			assertEquals(e, dbe);
 		}
+	}
+	
+	@Test
+	public void DBThrowableExceptionTest() {
+		DBException dbe = null;
+		try {
+			dbe = new DBException("Error", new Throwable());
+		} catch (concesionario.server.bd.DBException e) {
+			assertEquals(e, dbe);
+		}
+	}
+	
+
+	@Test
+	public void DBToStringTest() {
+		DBException dbe = null;
+		try {
+			dbe = new DBException("Error", new Throwable());
+			assertEquals("Error", dbe.toString());
+		} catch (concesionario.server.bd.DBException e) {
+		}
+		
 	}
 	
 	
