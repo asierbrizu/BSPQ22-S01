18ab88debcb61130bd308b08ca9a83314bcc4e7e
Iker Morales <ikermorales@opendeusto.es>
2022-05-02 00:33:51 +0200

    TestVentanaRegistro

---
+++ src/test/java/concesionario/clases/VentanaRegistroTest.java
@@ -0,0 +1,16 @@
+package concesionario.clases;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import concesionario.client.VentanaRegistro;
+
+public class VentanaRegistroTest {
+
+	@Test
+	public void test() {
+		VentanaRegistro vr = new VentanaRegistro();
+	}
+
+}
