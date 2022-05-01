bbfa164f71418fcd4aa3d9b352c882cbb51a1826
Iker Morales <ikermorales@opendeusto.es>
2022-05-02 00:35:28 +0200

    VentanaMetodoDePagoTest

---
+++ src/test/java/concesionario/clases/VentanaMetodoDePagoTest.java
@@ -0,0 +1,14 @@
+package concesionario.clases;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VentanaMetodoDePagoTest {
+
+	@Test
+	public void test() {
+		VentanaMetodoDePagoTest pago = new VentanaMetodoDePagoTest();
+	}
+
+}
