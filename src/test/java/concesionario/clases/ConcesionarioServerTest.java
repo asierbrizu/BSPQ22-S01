2062755757d9360a3bde6f0d58529f5ad354d5ec
AsierMM <alazne.parra@opendeusto.es>
2022-05-01 22:28:03 +0200

    Test Añadido

---
+++ src/test/java/concesionario/clases/ConcesionarioServerTest.java
@@ -0,0 +1,30 @@
+package concesionario.clases;
+
+import static org.junit.Assert.*;
+
+import javax.ws.rs.core.Response;
+
+import org.junit.Test;
+
+import concesionario.server.ConcesionarioServer;
+
+public class ConcesionarioServerTest {
+
+	ConcesionarioServer c = new ConcesionarioServer();
+	Compra c1 = new Compra("1", new Cliente("Asier", "Martinez", "31-12-2000", "12345678A", "pato@gmail.com"), "5143LMV", "01-05-2022", "70");
+
+	@Test
+	public void concesionarioServerTest() {
+	}
+
+	@Test
+	public void comprarCocheTest() {
+		c.comprarCoche(c1);
+	}
+
+	@Test
+	public void getComprasTest() {
+		c.getCompras();
+	}
+
+}
