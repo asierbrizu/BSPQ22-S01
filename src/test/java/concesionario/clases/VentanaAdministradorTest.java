aeae6bedf320eb4fcbe725743e1ad596e9462472
asierbrizu <asierbrizu@opendeusto.es>
2022-05-02 00:49:44 +0200

    Realizado test Ventana Administrador

---
+++ src/test/java/concesionario/clases/VentanaAdministradorTest.java
@@ -0,0 +1,16 @@
+package concesionario.clases;
+
+import static org.junit.Assert.*; 
+
+import org.junit.Test;
+
+import concesionario.client.VentanaAdministrador;
+
+public class VentanaAdministradorTest {
+
+	@Test
+	public void test() {
+		VentanaAdministrador vA = new VentanaAdministrador();
+	}
+
+}
